9aa4a97babfba97c0c4f8bd735e1a786c390f73d
Arten <artemmakarevich1997@gmail.com>
2021-09-04 14:06:33 +0300

    Second lab demonstration

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world");
+        // write your code here
+
+        Shape shape = new Shape(100, new Point[]{ new Point(), new Point(), new Point()});
+
+        System.out.println(shape);
+
+        BoundaryBox boundary = new BoundaryBox(shape);
+
+        System.out.println(boundary);
+
+        boundary.getShape().setColor(101);
+
+        System.out.println(shape);
 
     }
 }
