bb3ed5873c580b5d2d81f1b2bdb858b618b92ec0
Arten <artemmakarevich1997@gmail.com>
2021-09-14 09:23:26 +0300

    Added 4 lab explanation

---
+++ src/com/company/shape/Circle.java
@@ -0,0 +1,46 @@
+package com.company.shape;
+
+public class Circle extends Shape{
+    protected double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+
+
+    @Override
+    double getArea() {
+        return 0;
+    }
+
+    @Override
+    double getPerimetr() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
